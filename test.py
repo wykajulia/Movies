5c9c47e19a7d48bb8ec234c4f35bbcd5b864ff8d
wykajulia <39101824+wykajulia@users.noreply.github.com>
2019-11-03 17:49:15 +0100

    typo fix - function name

--- test.py
+++ test.py
@@ -3,13 +3,16 @@ import movies
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_colums_compare(self):
         o = movies.Movie()
         self.assertEqual(o.compare('Se7en','Memento', 'imdbRating'), "Comparison result = > The film Se7en: 8.6")
         self.assertEqual(o.compare('Green Book','Fargo', 'Year'), 'Comparison result = > The film Green Book: 2018')
         self.assertEqual(o.compare('Inception','Blade Runner', 'Runtime'), 'Comparison result = > The film Inception: 148 ' )
         self.assertEqual(o.compare('Jurassic Park','Room', 'Awards'), 'Comparison result => The film Room: Won 1 Oscar. Another 103 wins & 136 nominations.')
         self.assertEqual(o.compare('Coco','The Hunt', 'BoxOffice'), 'Comparison result = > The film Coco: 208,487,719')
+
+    def test_filter_by(self):
+        o = movies.Movie()
         output = [('The Usual Suspects', 'Bryan Singer')]
         self.assertEqual(o.filter_by('director','Bryan Singer'), output)
 
